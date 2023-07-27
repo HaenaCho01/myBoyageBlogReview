9be1876beeec5a16f3a9a5d4559ed7d2843837c9
Haena <johanna0123@naver.com>
2023-07-27 09:45:12 +0900

    init commit

---
+++ src/main/java/com/sparta/myvoyageblogreview/MyVoyageBlogReviewApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myvoyageblogreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVoyageBlogReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVoyageBlogReviewApplication.class, args);
+	}
+
+}
